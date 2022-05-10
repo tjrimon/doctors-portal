92ffdcd79fc70dd3649fc0b9cc12c1f82a613aa9
tjrimonm <tjrimonr@gmail.com>
2022-05-10 19:38:19 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Shared/Navbar';
 
 function App() {
   return (
     <div>
-
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
